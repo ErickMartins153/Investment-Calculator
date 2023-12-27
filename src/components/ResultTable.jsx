2ae45927a6478d99b01d7595028d005db001ae27
ErickMartins153 <121458723+ErickMartins153@users.noreply.github.com>
2023-12-26 21:01:29 -0300

    make the table tag a component

---
+++ src/components/ResultTable.jsx
@@ -0,0 +1,31 @@
+export default function ResultTable() {
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>1000</td>
+          <td>50</td>
+          <td>50</td>
+          <td>1050</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>1100</td>
+          <td>55</td>
+          <td>105</td>
+          <td>1155</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
