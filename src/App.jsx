8c90aaf527f9876fa4d17aed74ff46e51d734e9d
ErickMartins153 <121458723+ErickMartins153@users.noreply.github.com>
2023-12-26 21:01:53 -0300

    track the input change

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
+import InputField from "./components/InputField";
+import ResultTable from "./components/ResultTable";
+
 function App() {
+  const [inputs, setInputs] = useState({});
+
+  function handleInput(e, field) {
+    setInputs((prevInputs) => {
+      return { ...prevInputs, [field]: [e.target.value] };
+    });
+  }
+
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <InputField onChange={handleInput} />
+      <ResultTable />
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,22 +1,38 @@
-export default function InputField() {
+export default function InputField({ onChange }) {
   return (
     <section id="user-input">
       <div className="input-group">
         <p>
           <label>Initial Investment</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            onChange={(e) => onChange(e, "Initial Investment")}
+          />
         </p>
         <p>
           <label>Annual Investment</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            onChange={(e) => onChange(e, "Annual Investment")}
+          />
         </p>
         <p>
           <label>Expected Return</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            onChange={(e) => onChange(e, "Expected Return")}
+          />
         </p>
         <p>
           <label>Duration</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            onChange={(e) => onChange(e, "Duration")}
+          />
         </p>
       </div>
     </section>
